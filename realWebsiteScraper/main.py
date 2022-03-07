7266ec2d67729e81d0515ecb3d014f8928edb51b
Arnav Katgeri <arnav02@outlook.com>
2022-03-08 00:45:20 +0530

    jobSeeker initialized

--- realWebsiteScraper/main.py
+++ realWebsiteScraper/main.py
@@ -9,10 +9,11 @@ jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 for job in jobs:
     published_date = job.find(
         'span', class_='sim-posted').span.text.replace(' ', ' ')
-    if 'few' in published_date:
+    skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+    if 'few' in published_date and 'python' in skills:
         company_name = job.find(
             'h3', class_='joblist-comp-name').text.replace(' ', '')
-        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+
         print(f'''
       CompanyName: {company_name}
       Required Skills : {skills}
